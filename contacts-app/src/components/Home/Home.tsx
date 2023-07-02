3cfecbc8ec9abe814987d9eec8d3f23d18730571
Juan Maurente <juanmaurente@gmail.com>
2023-07-03 02:06:39 +1000

    added some coments on Home component

--- contacts-app/src/components/Home/Home.tsx
+++ contacts-app/src/components/Home/Home.tsx
@@ -16,37 +16,47 @@ const Home = () => {
 		useState<boolean>(false);
 
 	useEffect(() => {
+		// Fetch contacts data from API
 		axios
 			.get<Contact[]>('https://jsonplaceholder.typicode.com/users')
 			.then((response) => setContacts(response.data));
 	}, []);
 
 	const handleContact = (contact: Contact) => {
+		// Set the selected contact and show the contact display
 		setSelectedContact(contact);
 		setShowContactDisplay(true);
 	};
 
 	const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+		// Update the search query value
 		const { value } = event.target;
 		setSearchQuery(value);
 	};
 
 	const filteredContacts = contacts.filter((contact) =>
+		// Filter contacts based on the search query
 		contact.name.toLowerCase().includes(searchQuery.toLowerCase()),
 	);
 
 	const handleCloseContactDisplay = () => {
+		// Reset the selected contact and hide the contact display
 		setSelectedContact({} as Contact);
 		setShowContactDisplay(false);
 	};
 
 	return (
 		<div className='container'>
+			{/* Header component with search functionality */}
 			<Header handleSearch={handleSearch} />
+
+			{/* List of contacts */}
 			<ContactsList
 				contacts={filteredContacts}
 				setSelectedContact={handleContact}
 			/>
+
+			{/* Contact display */}
 			<ContactDisplay
 				selectedContact={selectedContact}
 				showContactDisplay={showContactDisplay}
